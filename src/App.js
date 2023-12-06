4562cf46029e5c9014f6c441496f03820b0d6121
superDev313 <pablo313boss@gmail.com>
2023-12-06 11:31:32 -0700

    make InputForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg'
 import './App.css'
 import { SideBar } from './components/SideBar'
-import { ContentDetails } from './components/ContentDetails'
-import { ContentNumber } from './components/ContentNumber'
 import { AddProduct } from './components/AddProduct'
+import { AddService } from './components/AddService'
 
 function App() {
   return (
@@ -11,16 +9,8 @@ function App() {
       {/* <SideBar /> */}
       <div className='min-w-[100px]'></div>
       <div className='grid grid-cols-2 gap-7 '>
-        {/* <div className='flex flex-col bg-[#3c3c3c66] p-[48px] rounded-[36px] mt-[183px] gap-[25px]'>
-          <ContentDetails />
-          <ContentNumber />
-          <div className='flex justify-center'>
-            <button className='px-[30px] py-[18px] bg-white text-[#232323] text-center text-Inter text-[15px] font-medium tracking-[-0.26px] rounded-[59px] min-w-[414px]'>
-              Onayla
-            </button>
-          </div>
-        </div> */}
         <AddProduct />
+        {/* <AddService /> */}
         <div className='flex'></div>
       </div>
     </div>
---
+++ src/components/basic/InputForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+// import { useState } from 'react/cjs/react.production.min'
+
+const InputForm = ({ label, errorMsg, placeholder }) => {
+  const [value, setValue] = useState('')
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value
+
+    setValue(inputValue)
+  }
+
+  return (
+    <div className='flex flex-col gap-[8px]'>
+      {label && <span className='text-[#ffffffcc] text-base font-medium'>{label}</span>}
+      <div className='flex flex-col gap-[8px]'>
+        <input
+          type='text'
+          value={value}
+          onChange={handleInputChange}
+          placeholder={placeholder}
+          className={
+            'px-[15px] py-[13px] w-full h-[43px] text-white rounded-[8px] border-[1.4px] border-[#ffffff33] bg-transparent placeholder:text-[#8F8F8F] placeholder:text-sm outline-none ' +
+            (!value ? 'border-red' : '')
+          }
+        />
+        {!value && <span className='text-[#FF6363] text-[13px] font-normal font-Inter'>{errorMsg}</span>}
+      </div>
+    </div>
+  )
+}
+
+export default InputForm
